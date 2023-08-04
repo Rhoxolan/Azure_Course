81f3cf8679ef2e54eab0b89436d1490abdc93abe
Rhoxolan <pba17mail@gmail.com>
2023-08-04 17:12:17 +0300

    The first practical work was began

---
+++ 2023.04.28_PW/Data/Contexts/ImagesContext.cs
@@ -0,0 +1,15 @@
+﻿using _2023._04._28_PW.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace _2023._04._28_PW.Data.Contexts
+{
+	public class ImagesContext : DbContext
+	{
+		public ImagesContext(DbContextOptions<ImagesContext> options) : base(options)
+		{
+			Database.EnsureCreated();
+		}
+
+		public DbSet<ImageEntity> ImageEntities { get; set; }
+	}
+}
---
+++ 2023.04.28_PW/Data/Entities/ImageEntity.cs
@@ -0,0 +1,11 @@
+﻿namespace _2023._04._28_PW.Data.Entities
+{
+	public class ImageEntity
+	{
+		public int Id { get; set; }
+
+		public string NameKey { get; set; } = default!;
+
+		public string Path { get; set; } = default!;
+	}
+}
