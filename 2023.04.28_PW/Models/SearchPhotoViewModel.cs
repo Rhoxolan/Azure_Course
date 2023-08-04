368cef06f9432fe1db31354ec71b937b26ad5df5
Rhoxolan <pba17mail@gmail.com>
2023-08-04 23:16:39 +0300

    The first practical work was supplemented

---
+++ 2023.04.28_PW/Models/SearchPhotoViewModel.cs
@@ -0,0 +1,7 @@
+﻿namespace _2023._04._28_PW.Models
+{
+	public class SearchPhotoViewModel
+	{
+		public string Name { get; set; } = default!;
+	}
+}
---
+++ 2023.04.28_PW/Models/UploadPhotoViewModel.cs
@@ -0,0 +1,7 @@
+﻿namespace _2023._04._28_PW.Models
+{
+	public class UploadPhotoViewModel
+	{
+		public IFormFile? Image { get; set; } = default!;
+	}
+}
