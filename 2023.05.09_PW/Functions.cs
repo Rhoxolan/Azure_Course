614b2c19985071e2462d211fa041c9ffb629ba52
Rhoxolan <pba17mail@gmail.com>
2023-09-17 19:38:39 +0300

    The third practical work was began

---
+++ 2023.05.09_PW/Functions.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Azure.WebJobs;
+using Microsoft.Extensions.Logging;
+namespace Functions
+{
+	public static class Functions
+	{
+		public static void ProcessQueueMessage([QueueTrigger("webjobs-test")] string message, ILogger logger)
+		{
+			logger.LogInformation($"File to process: {message}");
+		}
+
+		public static void ProcessBlobData([BlobTrigger("big-photos/{filename}")] Stream inputFile, string filename,
+			[Blob("small-photos/copy-{filename}", FileAccess.Write)] Stream outputFile, ILogger logger)
+		{
+			inputFile.CopyTo(outputFile);
+			logger.LogInformation($"File to process: {filename}");
+			logger.LogInformation($"{inputFile.Length} bytes");
+		}
+	}
+}
---
+++ 2023.05.09_PW/Program.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+HostBuilder builder = new HostBuilder();
+builder.ConfigureWebJobs(opt =>
+{
+	opt.AddAzureStorageBlobs();
+	opt.AddAzureStorageQueues();
+});
+builder.ConfigureLogging((HostBuilderContext context, ILoggingBuilder logBuilder) =>
+{
+	logBuilder.AddConsole();
+	logBuilder.AddApplicationInsightsWebJobs(config =>
+		config.InstrumentationKey = context.Configuration.GetSection("APPINSIGHTS_INSTRUMENTATIONSKEY").Value!);
+});
+IHost host = builder.Build();
+using (host)
+{
+	host.Run();
+}
