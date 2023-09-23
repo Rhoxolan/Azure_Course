d83d3b41559e5474430303e711ca31a7ac91073c
Rhoxolan <pba17mail@gmail.com>
2023-09-23 22:18:53 +0300

    The third practical work was supplemented

--- 2023.05.09_PW/Functions.cs
+++ 2023.05.09_PW/Functions.cs
@@ -6,20 +6,41 @@ namespace _2023._05._09_PW
 {
 	public static class Functions
 	{
-		public static void ProcessQueueMessage([QueueTrigger("webjobs-test")] string message, ILogger logger)
+		//Создание уменьшенной копии изображения фиксированного размера 300/200 при добавлении сообщения
+		//с именем изображения, которое находится основном контейнере
+		public static void ProcessQueueMessage([QueueTrigger("webjobs-test")] string message,
+			[Blob("big-photos/{queueTrigger}", FileAccess.Read)] Stream inputFile,
+			[Blob("small-photos/copy-{queueTrigger}", FileAccess.Write)] Stream outputFile, ILogger logger)
 		{
-			logger.LogInformation($"File to process: {message}");
+			try
+			{
+				using Image image = Image.Load(inputFile);
+				using Image imageThumbnail = image.Clone(i => i.Resize(300, 200));
+				imageThumbnail.Save(outputFile, imageThumbnail.Metadata.DecodedImageFormat!);
+				logger.LogInformation($"File to process: {message}");
+			}
+			catch (Exception ex)
+			{
+				logger.LogError(ex.Message);
+			}
 		}
 
 		//Создание уменьшенной копии изображения фиксированного размера 200/100 при первоначальной загрзуке изображения
 		public static void ProcessBlobData([BlobTrigger("big-photos/{filename}")] Stream inputFile, string filename,
 			[Blob("small-photos/copy-{filename}", FileAccess.Write)] Stream outputFile, ILogger logger)
 		{
-			using Image image = Image.Load(inputFile);
-			using Image imageThumbnail = image.Clone(i => i.Resize(200, 100));
-			imageThumbnail.Save(outputFile, imageThumbnail.Metadata.DecodedImageFormat!);
-			logger.LogInformation($"File to process: {filename}");
-			logger.LogInformation($"{inputFile.Length} bytes");
+			try
+			{
+				using Image image = Image.Load(inputFile);
+				using Image imageThumbnail = image.Clone(i => i.Resize(200, 100));
+				imageThumbnail.Save(outputFile, imageThumbnail.Metadata.DecodedImageFormat!);
+				logger.LogInformation($"File to process: {filename}");
+				logger.LogInformation($"{inputFile.Length} bytes");
+			}
+			catch(Exception ex)
+			{
+				logger.LogError(ex.Message);
+			}
 		}
 	}
 }
