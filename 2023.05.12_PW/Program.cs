a655d3d0a61670fcdb3fff1aec80b8a082ba6d88
Rhoxolan <pba17mail@gmail.com>
2023-11-18 22:50:49 +0200

    The fourth practical work was began

---
+++ 2023.05.12_PW/Program.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Azure.Cosmos;
+using Microsoft.Extensions.Configuration;
+
+namespace _2023._05._12_PW
+{
+	internal class Program
+	{
+		private static IConfigurationRoot configuration = new ConfigurationBuilder().AddUserSecrets<Program>().Build();
+
+		static async Task Main(string[] args)
+		{
+			Console.WriteLine("Hello, World!");
+		}
+
+
+		static async Task<Database> CreateAndGetDatabaseAsync()
+		{
+			var uri = configuration["COSMOS:URI"];
+			var key = configuration["COSMOS:PRIMARYKEY"];
+			var cosmosClient = new CosmosClient(uri, key);
+			return await cosmosClient.CreateDatabaseIfNotExistsAsync("TestDatabase");
+		}
+
+		static async Task<Container> CreateAndGetContainerAsync(Database database)
+		{
+			return await database.CreateContainerIfNotExistsAsync(id: "TestContainer", partitionKeyPath: "/Name");
+		}
+	}
+}
