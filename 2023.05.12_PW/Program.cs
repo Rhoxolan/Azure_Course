8de4207b8dbc311cc87b6d0341d09e08fb8c29bf
Rhoxolan <pba17mail@gmail.com>
2023-11-19 22:46:49 +0200

    The fourth practical work was completed

--- 2023.05.12_PW/Program.cs
+++ 2023.05.12_PW/Program.cs
@@ -16,10 +16,26 @@ namespace _2023._05._12_PW
 				Id = Guid.NewGuid().ToString(),
 				Name = "Test"
 			};
-			var item = await container.CreateItemAsync(item: entity, partitionKey: new(entity.Name)); ;
+			var item = await container.CreateItemAsync(item: entity, partitionKey: new(entity.Name));
 			Console.WriteLine($"The element with Id: {item.Resource.Id}" +
 				$" added to the database. " +
 				$"Complexity of the operation: {item.RequestCharge}");
+			string query = "SELECT * FROM c WHERE c.Name = 'Test'";
+			QueryDefinition queryDefinition = new(query);
+			FeedIterator<Entity> feedIterator = container.GetItemQueryIterator<Entity>(queryDefinition);
+			var entities = new List<Entity>();
+			while (feedIterator.HasMoreResults)
+			{
+				FeedResponse<Entity> feedResponse = await feedIterator.ReadNextAsync();
+				foreach(var responsedEntity in feedResponse)
+				{
+					entities.Add(responsedEntity);
+				}
+			}
+			foreach (var localResponsedEntity in entities)
+			{
+				Console.WriteLine(localResponsedEntity.Name);
+			}
 		}
 
 
