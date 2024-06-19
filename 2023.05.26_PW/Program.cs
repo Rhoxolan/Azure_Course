5cf3c33dda395cdeddae7705da914247aadd04bc
Rhoxolan <pba17mail@gmail.com>
2024-06-19 23:34:30 +0300

    Completed the nineth practical work

--- 2023.05.26_PW/Program.cs
+++ 2023.05.26_PW/Program.cs
@@ -15,6 +15,9 @@ namespace _2023._05._26_PW
 			Console.Write("Please, enter the key: ");
 			dto.Key = Console.ReadLine();
 
+			Console.Write("Please, enter the region value: ");
+			dto.Region = Console.ReadLine();
+
 			Console.Write("Please, enter the from value: ");
 			dto.From = Console.ReadLine();
 
@@ -24,7 +27,7 @@ namespace _2023._05._26_PW
 			Console.Write("Please, enter the api version: ");
 			dto.ApiVersion = Console.ReadLine();
 
-			Console.Write("Please, enter the thext to translate: ");
+			Console.Write("Please, enter the text to translate: ");
 			dto.Text = Console.ReadLine();
 
 			var result = await Translate(dto);
@@ -36,13 +39,15 @@ namespace _2023._05._26_PW
 		{
 			HttpClient client = new();
 
-			var uri = $"{dto.Uri}/translate?api-version={dto.ApiVersion}&from={dto.From}&to{dto.To}";
+			var uri = $"{dto.Uri}/translate?api-version={dto.ApiVersion}&from={dto.From}&to={dto.To}";
+			var body = new object[] { new { dto.Text } };
 
 			using HttpRequestMessage httpRequest = new(HttpMethod.Post, uri);
 
-			httpRequest.Content = new StringContent(JsonSerializer.Serialize(new { value = dto.Text }));
+			httpRequest.Content = new StringContent(JsonSerializer.Serialize(body));
 			httpRequest.Content.Headers.ContentType = new MediaTypeHeaderValue("application/json");
 			httpRequest.Headers.Add("Ocp-Apim-Subscription-Key", dto.Key);
+			httpRequest.Headers.Add("Ocp-Apim-Subscription-Region", dto.Region);
 
 			HttpResponseMessage response = await client.SendAsync(httpRequest);
 			return await response.Content.ReadAsStringAsync();
@@ -52,6 +57,8 @@ namespace _2023._05._26_PW
 	internal class AzureTranslateDTO
 	{
 		public string? Key { get; set; }
+
+		public string? Region { get; set; }
 
 		public string? Uri { get; set; }
 
